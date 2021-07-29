bb642da7e7475f4fb99028c4f37767bbedd0edee
Francisco Calderón <fjcalderon@gmail.com>
2021-07-29 10:53:12 -0300

    Add pending_payment model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const User = require('./user');
 const Order = require('./order');
+const PendingPayment = require('./pending_payment');
 
 module.exports = {
   User,
   Order,
+  PendingPayment,
 };
