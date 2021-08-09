cd8fda749f304088920a103bbdd1a509692a6ea1
Francisco Calderón <fjcalderon@gmail.com>
2021-08-09 16:58:38 -0300

    Adds tests

---
+++ tests/lightningResponse.js
@@ -0,0 +1,14 @@
+const createHodlInvoiceResponse = {
+    chain_address: undefined,
+    created_at: '2021-08-09T17:32:02.000Z',
+    description: 'Venta por @P2PLNBot',
+    id: '55bda326f001d427833b9e188471eadb24716c27269e3bb0a6fc55f4a645188d',
+    mtokens: '200000',
+    request: 'lnbcrt2u1ps3ze5jpp52k76xfhsq82z0qemncvggu02mvj8zmp8y60rhv9xl32lffj9rzxsdql2ejkuarpypcx7u3qgpgry5zvfepx7aqcqzpgxqyz5vqsp5hxvafxqp7ku4mknmuaqz6l0ahvs9eje9czr9wha96xr9evevz3nq9qyyssq7d9m0f82yql9rv6ayx7nxzk43nmm97m5qqwrj7qlfy09m7v2upvhn9vmj0r680llg3vs9k0luw3m5hrgxzq8umhdmn837nds8n2mz3cpqvs3a6',
+    secret: undefined,
+    tokens: 200,
+};
+
+module.exports = {
+    createHodlInvoiceResponse,
+};
