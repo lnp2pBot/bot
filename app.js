b88bcb4bf6dad73adb302c44651cb87295bf1553
Francisco Calderón <fjcalderon@gmail.com>
2021-08-11 10:51:18 -0300

    Refactoring bot directory

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
-const { Telegraf } = require('telegraf');
-const botStart = require('./bot');
+const { start } = require('./bot');
 const mongoConnect = require('./db_connect');
 
 mongoConnect();
-botStart();
+start();
