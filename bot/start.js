942cde29d5813455dc1b515fded45a0eec3be13e
Francisco Calderón <fjcalderon@gmail.com>
2021-08-25 11:11:15 -0300

    Delete order messages on channel
    In order to have a clean channel we delete taken or canceled orders

--- bot/start.js
+++ bot/start.js
@@ -276,11 +276,9 @@ const start = () => {
       await order.save();
       // we sent a private message to the user
       await messages.customMessage(bot, user, `Has cancelado la orden Id: ${order._id}!`);
-      // we update this order message in the channel
-      await bot.telegram.editMessageText(process.env.CHANNEL, order.tg_channel_message2, null, `Orden ${order._id} CANCELADA ❌`);
-      if (order.tg_chat_id < 0) {
-        await bot.telegram.editMessageText(order.tg_chat_id, order.tg_group_message2, null, `Orden ${order._id} CANCELADA ❌`);
-      }
+      // We delete the messages related to that order from the channel
+      await bot.telegram.deleteMessage(process.env.CHANNEL, order.tg_channel_message1);
+      await bot.telegram.deleteMessage(process.env.CHANNEL, order.tg_channel_message2);
     } catch (error) {
       console.log(error);
     }
@@ -314,11 +312,9 @@ const start = () => {
       await messages.customMessage(bot, seller, `El admin ha cancelado la orden Id: ${order._id}!`);
       // we sent a private message to the buyer
       await messages.customMessage(bot, buyer, `El admin cancelado la orden Id: ${order._id}!`);
-      // we update this order message in the channel
-      await bot.telegram.editMessageText(process.env.CHANNEL, order.tg_channel_message2, null, `Orden ${order._id} CANCELADA ❌`);
-      if (order.tg_chat_id < 0) {
-        await bot.telegram.editMessageText(order.tg_chat_id, order.tg_group_message2, null, `Orden ${order._id} CANCELADA ❌`);
-      }
+      // We delete the messages related to that order from the channel
+      await bot.telegram.deleteMessage(process.env.CHANNEL, order.tg_channel_message1);
+      await bot.telegram.deleteMessage(process.env.CHANNEL, order.tg_channel_message2);
     } catch (error) {
       console.log(error);
     }
@@ -473,11 +469,9 @@ const start = () => {
         // We sent a private message to the users
         await messages.customMessage(bot, initiatorUser, `Has cancelado la orden Id: ${order._id}!`);
         await messages.customMessage(bot, counterPartyUser, `Tu contraparte ha estado de acuerdo y ha sido cancelada la orden Id: ${order._id}!`);
-        // We update this order message in the channel
-        await bot.telegram.editMessageText(process.env.CHANNEL, order.tg_channel_message2, null, `Orden ${order._id} CANCELADA ❌`);
-        if (order.tg_chat_id < 0) {
-          await bot.telegram.editMessageText(order.tg_chat_id, order.tg_group_message2, null, `Orden ${order._id} CANCELADA ❌`);
-        }
+        // We delete the messages related to that order from the channel
+        await bot.telegram.deleteMessage(process.env.CHANNEL, order.tg_channel_message1);
+        await bot.telegram.deleteMessage(process.env.CHANNEL, order.tg_channel_message2);
       } else {
         await messages.customMessage(bot, initiatorUser, `Has iniciado la cancelación de la orden Id: ${order._id}, tu contraparte también debe indicarme que desea cancelar la orden`);
         await messages.customMessage(bot, counterPartyUser, `Tu contraparte quiere cancelar la orden Id: ${order._id}, si estás de acuerdo utiliza el comando 👇`);
