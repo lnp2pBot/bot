d1e43acaa6b4b1bb3665d2834d72361075acb189
Francisco Calderón <fjcalderon@gmail.com>
2021-09-28 15:56:06 -0300

    Add trycatch to scene

--- bot/scenes.js
+++ bot/scenes.js
@@ -14,46 +14,49 @@ const addInvoiceWizard = new Scenes.WizardScene(
     return ctx.wizard.next();
   },
   async (ctx) => {
-    const lnInvoice = ctx.message.text;
-    const res = await isValidInvoice(lnInvoice);
-    console.log(res)
-    if (!res.success) {
-      ctx.reply(res.error);
-      return;
-    };
-    const { bot, buyer, seller, order } = ctx.wizard.state;
-
-    if (res.invoice.tokens && res.invoice.tokens != order.amount) {
-      ctx.reply('La factura tiene un monto incorrecto');
-      return;
+    try {
+      const lnInvoice = ctx.message.text;
+      const res = await isValidInvoice(lnInvoice);
+      if (!res.success) {
+        ctx.reply(res.error);
+        return;
+      };
+      const { bot, buyer, seller, order } = ctx.wizard.state;
+  
+      if (res.invoice.tokens && res.invoice.tokens != order.amount) {
+        ctx.reply('La factura tiene un monto incorrecto');
+        return;
+      }
+      order.buyer_invoice = lnInvoice;
+      // If the buyer is the creator, at this moment the seller already paid the hold invoice
+      if (order.creator_id == order.buyer_id) {
+        const currency = getCurrency(order.fiat_code);
+        await bot.telegram.sendMessage(buyer.tg_id, `🤖 He recibido tu factura, ponte en contacto con @${seller.username} para que te indique como enviarle ${currency.symbol_native} ${order.fiat_amount}`);
+        await bot.telegram.sendMessage(buyer.tg_id, `En cuanto hayas enviado el dinero fiat hazmelo saber con el comando 👇`);
+        await bot.telegram.sendMessage(buyer.tg_id, `/fiatsent ${order._id}`);
+      } else {
+        // We create a hold invoice
+        const description = `Venta por @${ctx.botInfo.username}`;
+        const amount = Math.floor(order.amount + order.fee);
+        const { request, hash, secret } = await createHoldInvoice({
+          amount,
+          description,
+        });
+        order.hash = hash;
+        order.secret = secret;
+        order.taken_at = Date.now();
+        order.status = 'WAITING_PAYMENT';
+        // We monitor the invoice to know when the seller makes the payment
+        await subscribeInvoice(bot, hash);
+        // We send the hold invoice to the seller
+        await messages.invoicePaymentRequestMessage(bot, seller, request);
+        await messages.takeSellWaitingSellerToPayMessage(bot, buyer, order);
+      }
+      await order.save();
+      return ctx.scene.leave();
+    } catch (error) {
+      console.log(error);
     }
-    order.buyer_invoice = lnInvoice;
-    // If the buyer is the creator, the seller already paid the hold invoice
-    if (order.creator_id == order.buyer_id) {
-      const currency = getCurrency(order.fiat_code);
-      await bot.telegram.sendMessage(buyer.tg_id, `🤖 He recibido tu factura, ponte en contacto con @${seller.username} para que te indique como enviarle ${currency.symbol_native} ${order.fiat_amount}`);
-      await bot.telegram.sendMessage(buyer.tg_id, `En cuanto hayas enviado el dinero fiat hazmelo saber con el comando 👇`);
-      await bot.telegram.sendMessage(buyer.tg_id, `/fiatsent ${order._id}`);
-    } else {
-      // We create a hold invoice
-      const description = `Venta por @${ctx.botInfo.username}`;
-      const amount = Math.floor(order.amount + order.fee);
-      const { request, hash, secret } = await createHoldInvoice({
-        amount,
-        description,
-      });
-      order.hash = hash;
-      order.secret = secret;
-      order.taken_at = Date.now();
-      order.status = 'WAITING_PAYMENT';
-      // We monitor the invoice to know when the seller makes the payment
-      await subscribeInvoice(bot, hash);
-      // We send the hold invoice to the seller
-      await messages.invoicePaymentRequestMessage(bot, seller, request);
-      await messages.takeSellWaitingSellerToPayMessage(bot, buyer, order);
-    }
-    await order.save();
-    return ctx.scene.leave();
   },
 );
 
