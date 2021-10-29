2440f561cc66a1246908b7cf12da6eed4d40ebd4
Francisco Calderón <fjcalderon@gmail.com>
2021-10-29 15:24:08 -0300

    Output errors to console
    Output unhandledRejection and uncaughtException errors to console

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ require('dotenv').config();
 const { start } = require('./bot');
 const mongoConnect = require('./db_connect');
 
+process.on('unhandledRejection', e => { console.log (e); });
+
+process.on('uncaughtException', e => { console.log (e); });
+
 mongoConnect();
 start(process.env.BOT_TOKEN);
