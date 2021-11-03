8d9eadc5d6a8a3ec0f89b91eb4156999e7fb15fd
Francisco Calderón <fjcalderon@gmail.com>
2021-11-03 10:25:03 -0300

    Show payment method on messages from the bot

--- bot/scenes.js
+++ bot/scenes.js
@@ -47,7 +47,7 @@ const addInvoiceWizard = new Scenes.WizardScene(
       if (order.creator_id == order.buyer_id) {
         order.status = 'ACTIVE';
         const currency = getCurrency(order.fiat_code);
-        await bot.telegram.sendMessage(buyer.tg_id, `🤖 He recibido tu factura, ponte en contacto con @${seller.username} para que te indique como enviarle ${currency.symbol_native} ${order.fiat_amount}`);
+        await bot.telegram.sendMessage(buyer.tg_id, `🤖 He recibido tu factura, ponte en contacto con @${seller.username} para que te indique como enviarle ${currency.symbol_native} ${order.fiat_amount} por ${order.payment_method}`);
         await bot.telegram.sendMessage(buyer.tg_id, `En cuanto hayas enviado el dinero fiat hazmelo saber con el comando 👇`);
         await bot.telegram.sendMessage(buyer.tg_id, `/fiatsent ${order._id}`);
       } else {
