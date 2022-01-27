d8507ee1879052dfdf6d219b53b2737240be4b12
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2022-01-27 00:50:58 -0300

    Refactor createOrder function

--- bot/ordersActions.js
+++ bot/ordersActions.js
@@ -6,108 +6,123 @@ const { getCurrency, getBtcExchangePrice, getEmojiRate, decimalRound } = require
 const createOrder = async (ctx, bot, user, {
   type,
   amount,
-  seller,
-  buyer,
   fiatAmount,
   fiatCode,
   paymentMethod,
   status,
   priceMargin,
 }) => {
-  amount = parseInt(amount);
-  const action = type == 'sell' ? 'Vendiendo' : 'Comprando';
-  const trades = type == 'sell' ? seller.trades_completed : buyer.trades_completed;
-  const volume = type == 'sell' ? seller.volume_traded : buyer.volume_traded;
-  const totalRating = type == 'sell' ? seller.total_rating : buyer.total_rating;
-  const totalReviews = type == 'sell' ? seller.reviews.length : buyer.reviews.length;
   try {
+    amount = parseInt(amount);
+    const fee = amount * parseFloat(process.env.FEE);
+    const currency = getCurrency(fiatCode);
+    const priceFromAPI = !amount;
+
+    if (priceFromAPI && !currency.price) {
+      await messages.notRateForCurrency(bot, user);
+      return;
+    }
+
+    const baseOrderData = {
+      amount,
+      fee,
+      creator_id: user._id,
+      type,
+      status,
+      fiat_amount: fiatAmount,
+      fiat_code: fiatCode,
+      payment_method: paymentMethod,
+      tg_chat_id: ctx.message.chat.id,
+      tg_order_message: ctx.message.message_id,
+      price_from_api: priceFromAPI,
+      price_margin: priceMargin || 0,
+      description: buildDescription({
+        user,
+        type,
+        amount,
+        fiatAmount,
+        fiatCode,
+        paymentMethod,
+        priceMargin,
+        priceFromAPI,
+        currency
+      })
+    };
+
+    let order;
+
+    if (type === 'sell') {
+      order = new Order({
+        seller_id: user._id,
+        ...baseOrderData,
+      });
+    } else {
+      order = new Order({
+        buyer_id: user._id,
+        ...baseOrderData,
+      });
+    }
+    await order.save();
+
+    return order;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const buildDescription = ({
+  user,
+  type,
+  amount,
+  fiatAmount,
+  fiatCode,
+  paymentMethod,
+  priceMargin,
+  priceFromAPI,
+  currency,
+}) => {
+  try {
+    const action = type == 'sell' ? 'Vendiendo' : 'Comprando';
+    const paymentAction = type == 'sell' ? 'Recibo pago' : 'Pago';
+    const trades = user.trades_completed;
+    const volume = user.volume_traded;
+    const totalRating = user.total_rating;
+    const totalReviews = user.reviews.length;
     const username = user.show_username ? `@${user.username} está ` : ``;
     const volumeTraded = user.show_volume_traded ? `Volumen de comercio: ${volume} sats\n` : ``;
     priceMargin = (!!priceMargin && priceMargin > 0) ? `+${priceMargin}` : priceMargin;
     const priceMarginText = !!priceMargin ? `${priceMargin}%\n` : ``;
-    const currency = getCurrency(fiatCode);
     let currencyString = `${fiatCode} ${fiatAmount}`;
+  
     if (!!currency) {
       currencyString = `${fiatAmount} ${currency.name_plural} ${currency.emoji}`;
     }
+    
     let amountText = `${amount} `;
     let tasaText = '';
-    let rateText = '';
-    let priceFromAPI = false;
-    if (amount == 0) {
-      priceFromAPI = true;
-      if (!currency.price) {
-        await messages.notRateForCurrency(bot, user);
-        return;
-      }
+    if (priceFromAPI) {
       amountText = '';
       tasaText = `Tasa: ${process.env.FIAT_RATE_NAME} ${priceMarginText}\n`;
     } else {
       const exchangePrice = getBtcExchangePrice(fiatAmount, amount);
       tasaText = `Precio: ${exchangePrice.toFixed(2)}\n`
     }
-
+  
+    let rateText = '';
     if (!!totalRating) {
       const stars = getEmojiRate(totalRating);
       const roundedRating = decimalRound(totalRating, -1);
       rateText = `${roundedRating} ${stars} (${totalReviews})\n`;
     }
-
-    if (type === 'sell') {
-      const fee = amount * parseFloat(process.env.FEE);
-      let description = `${username}${action} ${amountText}sats\nPor ${currencyString}\n`;
-      description += `Recibo pago por ${paymentMethod}\n`;
-      description += `Tiene ${trades} operaciones exitosas\n`;
-      description += volumeTraded;
-      description += tasaText;
-      description += rateText;
-      const order = new Order({
-        description,
-        amount,
-        fee,
-        creator_id: seller._id,
-        seller_id: seller._id,
-        type,
-        status,
-        fiat_amount: fiatAmount,
-        fiat_code: fiatCode,
-        payment_method: paymentMethod,
-        tg_chat_id: ctx.message.chat.id,
-        tg_order_message: ctx.message.message_id,
-        price_from_api: priceFromAPI,
-        price_margin: priceMargin || 0,
-      });
-      await order.save();
-
-      return order;
-    } else {
-      const fee = amount * parseFloat(process.env.FEE);
-      let description = `${username}${action} ${amountText}sats\nPor ${currencyString}\n`;
-      description += `Pago por ${paymentMethod}\n`;
-      description += `Tiene ${trades} operaciones exitosas\n`;
-      description += volumeTraded;
-      description += tasaText;
-      description += rateText;
-      const order = new Order({
-        description,
-        amount,
-        fee,
-        creator_id: buyer._id,
-        buyer_id: buyer._id,
-        type,
-        fiat_amount: fiatAmount,
-        fiat_code: fiatCode,
-        payment_method: paymentMethod,
-        status,
-        tg_chat_id: ctx.message.chat.id,
-        tg_order_message: ctx.message.message_id,
-        price_from_api: priceFromAPI,
-        price_margin: priceMargin || 0,
-      });
-      await order.save();
-
-      return order;
-    }
+  
+    let description = `${username}${action} ${amountText}sats\nPor ${currencyString}\n`;
+    description += `${paymentAction} por ${paymentMethod}\n`;
+    description += `Tiene ${trades} operaciones exitosas\n`;
+    description += volumeTraded;
+    description += tasaText;
+    description += rateText;
+  
+    return description
   } catch (e) {
     console.log(e);
   }
