08e1129b66ec2fa4c3a79d5b0a2d5c7e1a6d5028
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2022-02-07 20:35:32 -0300

    Add logic to relaunch range orders
    The idea behind range orders is to relaunch them after completion of a
    succesfful transaction related to the range order.
    
    The new upper limit of the range order will be computed with the
    following formula:
    `new_max_amount = max_amount - selected_amount`
    
    Range orders will be relaunched only if the `new_max_amount` is above
    the min amount provided in the original range order.

--- ln/pay_request.js
+++ ln/pay_request.js
@@ -4,6 +4,7 @@ const { User, PendingPayment } = require('../models');
 const lnd = require('./connect');
 const { handleReputationItems } = require('../util');
 const messages = require('../bot/messages');
+const ordersActions = require('../bot/ordersActions');
 
 const payRequest = async ({ request, amount }) => {
   try {
@@ -52,6 +53,43 @@ const payToBuyer = async (bot, order) => {
       console.log(`Invoice with hash: ${payment.id} paid`);
       order.status = 'SUCCESS';
       order.routing_fee = payment.fee;
+      let newOrderPayload = undefined;
+      let newMaxAmount = 0;
+
+      if (order.max_amount !== undefined) {
+        newMaxAmount = order.max_amount - order.fiat_amount;
+      }
+      
+      if (newMaxAmount > order.min_amount) {
+        const orderData = {
+          type: order.type,
+          amount: 0,
+          // drop newMaxAmount if it is equal to min_amount and create a
+          // not range order.
+          // Set preserves insertion order, so min_amount will be always
+          // before newMaxAmount
+          fiatAmount: [...new Set([order.min_amount, newMaxAmount])],
+          fiatCode: order.fiat_code,
+          paymentMethod: order.payment_method,
+          status: 'PENDING',
+          priceMargin: order.price_margin,
+        };
+
+        const orderCtx = {
+          message: {
+            chat: {
+              id: order.tg_chat_id,
+            },
+            message_id: order.tg_order_message,
+          }
+        };
+
+        newOrderPayload = {orderData, orderCtx};
+        
+        order.max_amount = undefined;
+        order.min_amount = undefined;
+      }
+
       await order.save();
       // TODO: We need to fix this, the seller should get reputation just after release
       await handleReputationItems(buyerUser, sellerUser, order.amount);
@@ -60,6 +98,22 @@ const payToBuyer = async (bot, order) => {
         messages.rateUserMessage(bot, buyerUser, order),
         messages.rateUserMessage(bot, sellerUser, order),
       ]);
+
+      if (!!newOrderPayload) {
+        let user;
+        if (order.type === 'sell') {
+          user = await User.findOne({ _id: order.seller_id });
+        } else {
+          user = await User.findOne({ _id: order.buyer_id });
+        }
+        const { orderCtx, orderData } = newOrderPayload;
+        const newOrder = await ordersActions.createOrder(orderCtx, bot, user, orderData);
+  
+        if (!!newOrder) {
+          await messages.publishBuyOrderMessage(bot, newOrder);
+          await messages.pendingBuyMessage(bot, user, newOrder);
+        }
+      }
     } else {
       await messages.invoicePaymentFailedMessage(bot, buyerUser);
       const pp = new PendingPayment({
