c69bf4366d6a66bdaef8c4acbe19d9e5768bf665
Francisco <fjcalderon@gmail.com>
2022-05-02 16:36:17 -0300

    Add currencies to community schema

--- models/community.js
+++ models/community.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const OrderChannelSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, trim: true },
   type: {
     type: String,
     enum: ['buy', 'sell', 'mixed'],
@@ -10,24 +10,34 @@ const OrderChannelSchema = new mongoose.Schema({
 
 const usernameIdSchema = new mongoose.Schema({
   id: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, trim: true },
 });
 
 const arrayLimits = (val) => {
   return val.length > 0 && val.length <= 2;
 }
 
+const currencyLimits = (val) => {
+  return val.length > 0 && val.length < 10;
+}
+
 const CommunitySchema = new mongoose.Schema({
-  name: { type: String, unique: true, maxlength: 20, trim: true, required: true },
+  name: { type: String, unique: true, maxlength: 30, trim: true, required: true },
   creator_id: { type: String },
-  group: { type: String }, // group Id or public name
+  group: { type: String, trim: true }, // group Id or public name
   order_channels: { // array of Id or public name of channels
     type: [OrderChannelSchema],
-    validate: [arrayLimits, '{PATH} is not within limits']
+    validate: [arrayLimits, '{PATH} is not within limits'],
   },
   dispute_channel: { type: String }, // Id or public name, channel to send new disputes
   solvers: [usernameIdSchema], // users that are dispute solvers
   public: { type: Boolean, default: true },
+  currencies: {
+    type: [String],
+    required: true,
+    trim: true,
+    validate: [currencyLimits, '{PATH} is not within limits']
+  },
   created_at: { type: Date, default: Date.now },
 });
 
