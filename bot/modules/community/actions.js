483410d08ea18086494f1ba9d0b405ec47f99c87
Axel Dolce <axel.dolce@gmail.com>
2022-05-16 19:50:19 -0300

    #232 button table

--- bot/modules/community/actions.js
+++ bot/modules/community/actions.js
@@ -1,23 +1,35 @@
 //@ts-check
-const { Community, User } = require('../../../models')
+const { Community, Order, User } = require('../../../models')
 const messages = require('../../messages')
 
 exports.onCommunityInfo = async ctx => {
     const commId = ctx.match[1]
     const community = await Community.findById(commId)
+    const userCount = await User.count({ default_community_id: commId })
+    const orderCount = await Order.count({ community_id: commId })
+    
+    const row0 = [
+        { text: 'Orders 24hs', callback_data: `none` },
+        { text: orderCount, callback_data: `none` }
+    ]
+    const row1 = [
+        { text: 'Users', callback_data: `none` },
+        { text: userCount, callback_data: `none` }
+    ]
     const buttons = [{
-        text: '/setcomm',
+        text: 'Utilizar por defecto',
         callback_data: `setCommunity_${commId}`
     }]
-    await ctx.reply(community.name, {
-        reply_markup: { inline_keyboard: [buttons] }
+    const text = `${community.name}\n@${community.group}`
+    await ctx.reply(text, {
+        reply_markup: { inline_keyboard: [row0, row1, buttons] }
     })
 }
 
 exports.onSetCommunity = async ctx => {
     const tg_id = ctx.update.callback_query.from.id
-    const default_community_id = ctx.match[1] + new Date().toISOString()
+    const default_community_id = ctx.match[1]
     const user = await User.findOneAndUpdate({ tg_id }, { default_community_id })
-    await ctx.reply(default_community_id + '\n' + JSON.stringify(user))
+    //await ctx.reply(default_community_id + '\n' + JSON.stringify(user))
     await messages.operationSuccessfulMessage(ctx)
 }
--- bot/modules/community/commands.js
+++ bot/modules/community/commands.js
@@ -1,34 +1,43 @@
 //@ts-check
 const logger = require('../../../logger');
-const { Community, Order } = require("../../../models");
+const { Community, Order, User } = require("../../../models");
 const { parseArgs, getCurrency } = require("../../../util");
 const { validateUser } = require("../../validations");
 
 async function findCommunities(currency) {
     const communities = await Community.find({ currencies: currency })
+    //const userCount = await getUserCountByCommunity()
     const orderCount = await getOrderCountByCommunity()
     return communities.map(comm => {
+        //comm.users = userCount[comm.id] || 0
         comm.orders = orderCount[comm.id] || 0
         return comm
-    }).sort((a, b) => b.orders - a.orders)
+    })
 }
 
 exports.findCommunity = async ctx => {
     try {
         const user = await validateUser(ctx, false)
         if (!user) return
-        
+
         const [com, u_fiatCode] = parseArgs(ctx.message.text)
         const currency = getCurrency(u_fiatCode.toUpperCase())
         if (!currency) return ctx.reply('InvalidCurrencyCode')
 
         const communities = await findCommunities(currency.code)
+        communities.sort((a, b) => a.orders - b.orders)
 
-        const inline_keyboard = communities.map(comm => [{
-            text: `@${comm.group} | ${comm.name} | ${comm.currencies.join(',')} | ${comm.orders}`,
-            callback_data: `communityInfo_${comm._id}`
-        }])
-        const res = await ctx.reply('id | name | currencies | orders', {
+        const inline_keyboard = []
+        while (communities.length > 0) {
+            const lastTwo = communities.splice(-2)
+            const lineBtn = lastTwo.reverse().map(comm => ({
+                text: `${comm.name}`,
+                callback_data: `communityInfo_${comm._id}`
+            }))
+            inline_keyboard.push(lineBtn)
+        }
+        const text = 'Selecciona la comunidad'
+        const res = await ctx.reply(text, {
             reply_markup: { inline_keyboard }
         })
     } catch (error) {
@@ -39,6 +48,15 @@ exports.findCommunity = async ctx => {
 async function getOrderCountByCommunity() {
     const data = await Order.aggregate([
         { $group: { _id: "$community_id", total: { $count: {} } } }
+    ])
+    return data.reduce((sum, item) => {
+        sum[item._id] = item.total
+        return sum
+    }, {})
+}
+async function getUserCountByCommunity() {
+    const data = await User.aggregate([
+        { $group: { _id: "$default_community_id", total: { $count: {} } } }
     ])
     return data.reduce((sum, item) => {
         sum[item._id] = item.total
