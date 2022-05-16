a143a7bc77e52638ae60464854c456208a18c630
Axel Dolce <axel.dolce@gmail.com>
2022-05-16 20:14:02 -0300

    #232 get 24h volume

--- bot/modules/community/actions.js
+++ bot/modules/community/actions.js
@@ -7,22 +7,28 @@ exports.onCommunityInfo = async ctx => {
     const community = await Community.findById(commId)
     const userCount = await User.count({ default_community_id: commId })
     const orderCount = await Order.count({ community_id: commId })
-    
-    const row0 = [
+    const volume = await getVolume24hs(commId)
+
+    const rows = []
+    rows.push([
         { text: 'Orders 24hs', callback_data: `none` },
         { text: orderCount, callback_data: `none` }
-    ]
-    const row1 = [
+    ])
+    rows.push([
+        { text: 'Volume 24hs', callback_data: `none` },
+        { text: 'unknown', callback_data: `none` }
+    ])
+    rows.push([
         { text: 'Users', callback_data: `none` },
         { text: userCount, callback_data: `none` }
-    ]
-    const buttons = [{
+    ])
+    rows.push([{
         text: 'Utilizar por defecto',
         callback_data: `setCommunity_${commId}`
-    }]
+    }])
     const text = `${community.name}\n@${community.group}`
     await ctx.reply(text, {
-        reply_markup: { inline_keyboard: [row0, row1, buttons] }
+        reply_markup: { inline_keyboard: rows }
     })
 }
 
@@ -32,4 +38,28 @@ exports.onSetCommunity = async ctx => {
     const user = await User.findOneAndUpdate({ tg_id }, { default_community_id })
     //await ctx.reply(default_community_id + '\n' + JSON.stringify(user))
     await messages.operationSuccessfulMessage(ctx)
+}
+
+async function getVolume24hs(commId) {
+    const now = new Date()
+    const yesterday = new Date()
+    yesterday.setHours(now.getHours() - 24)
+    const rows = await Order.aggregate([{
+        $match: {
+            status: 'SUCCESS',
+            created_at: {
+                $gte: yesterday.toISOString(),
+                $lte: now.toISOString()
+            },
+            community_id: commId
+        }
+    }, {
+        $group: {
+            _id: null,
+            amount: { $sum: "$amount" },
+            routing_fee: { $sum: "$routing_fee" },
+            fee: { $sum: "$fee" },
+        }
+    }])
+    return rows
 }
