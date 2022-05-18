3f7299a2bf17440dafc7cbcd47df0ccef33bf381
Axel Dolce <axel.dolce@gmail.com>
2022-05-18 11:56:34 -0300

    #232 renamed command

--- bot/modules/community/index.js
+++ bot/modules/community/index.js
@@ -2,7 +2,7 @@ const actions = require('./actions')
 const commands = require('./commands')
 
 exports.configure = bot => {
-    bot.command('findcomm', commands.findCommunity)
+    bot.command('findcomms', commands.findCommunity)
 
     bot.action(/^communityInfo_([0-9a-f]{24})$/, actions.onCommunityInfo)
     bot.action(/^setCommunity_([0-9a-f]{24})$/, actions.onSetCommunity)
