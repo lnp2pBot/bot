fe1fdd98385950dd93e0ce134b464cd3c5dc95ef
Axel Dolce <axel.dolce@gmail.com>
2022-05-19 16:36:49 -0300

    try fix github alert

--- bot/modules/orders/messages.js
+++ bot/modules/orders/messages.js
@@ -1,12 +1,14 @@
 const { getOrderChannel } = require('../../../util');
 
+const ESCAPED_UNDERSCORE = '\\_'
+
 exports.listOrdersResponse = async (orders) => {
   const response = '             Id          \\|     Status    \\|   sats amount  \\|  fiat amt  \\|  fiat\n';
   const tasks = orders.map(async (order) => {
     const channel = await getOrderChannel(order);
     let fiatAmount = '\\-';
     let amount = '\\-';
-    const status = order.status.replace(/_/g, '\\_');
+    const status = order.status.replace(/_/g, ESCAPED_UNDERSCORE);
     if (typeof order.fiat_amount !== 'undefined') fiatAmount = order.fiat_amount;
     if (typeof order.amount !== 'undefined') amount = order.amount;
     return `\`${order.id}\` \\| ${status} \\| ${amount} \\| ${fiatAmount} \\| ${order.fiat_code} \\| ${channel}`;
