a08e1e4d089e7ff91fcd96205d57e3070065bbdb
Axel Dolce <axel.dolce@gmail.com>
2023-03-14 14:14:03 -0300

    snake_case & i18n(es/en)

--- bot/modules/nostr/index.js
+++ bot/modules/nostr/index.js
@@ -37,9 +37,9 @@ exports.configure = bot => {
       const [, npub] = ctx.message.text.trim().split(' ');
       const { type, data } = Nostr.nip19.decode(npub);
       if (type !== 'npub') throw new Error('InvalidNpub');
-      ctx.user.nostrPublicKey = data;
+      ctx.user.nostr_public_key = data;
       await ctx.user.save();
-      await ctx.reply(`user.nostrPublicKey = ${data}`);
+      await ctx.reply(ctx.i18n.t('user_npub_updated', { npub }));
     } catch (err) {
       logger.error(err);
       return ctx.reply('Error');
--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
   show_username: { type: Boolean, default: false },
   show_volume_traded: { type: Boolean, default: false },
   lightning_address: { type: String },
-  nostrPublicKey: { type: String },
+  nostr_public_key: { type: String },
   disputes: { type: Number, min: 0, default: 0 },
   created_at: { type: Date, default: Date.now },
   default_community_id: { type: String },
