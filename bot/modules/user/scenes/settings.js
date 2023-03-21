7717fe5193e7d50541c88eef00be894089898457
Axel Dolce <axel.dolce@gmail.com>
2023-03-21 17:42:50 -0300

    i18n

--- bot/modules/user/scenes/settings.js
+++ bot/modules/user/scenes/settings.js
@@ -1,4 +1,5 @@
 const { Scenes } = require('telegraf');
+const { getLanguageFlag } = require('../../../../util');
 const NostrCommands = require('../../nostr/commands');
 
 function make() {
@@ -8,7 +9,13 @@ function make() {
   };
   const scene = new Scenes.WizardScene('USER_SETTINGS', async ctx => {
     const { user } = ctx.scene.state;
-    const str = ctx.i18n.t('user_admin', { user });
+    const language = getLanguageFlag(user.lang || ctx.from?.language_code);
+    const str = ctx.i18n.t('user_settings', { user, language });
+    await ctx.reply(str, { parse_mode: 'HTML' });
+  });
+
+  scene.command('/help', async ctx => {
+    const str = ctx.i18n.t('user_settings_help');
     await ctx.reply(str, { parse_mode: 'HTML' });
   });
 
