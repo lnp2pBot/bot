d9faf609c5468c20a4e4c980c4ed780e4eea3e19
Nelson <bilthon@gmail.com>
2023-07-30 13:32:05 -0500

    Introduces simpler LND connection method
    It's a lot simpler to just copy & paste 'tls.cert' & 'admin.macaroon'
    than having to mess with their contents and expose them as environment
    variables. The previous method still works though.
    
    The 'tls.cert' and 'admin.macaroon' files were thus added to the
    .gitignore
    
    Also adds more logs to facilitate troubleshooting.

--- db_connect.js
+++ db_connect.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const logger = require('./logger');
 
 // connect to database
 const credentials = process.env.DB_USER
@@ -10,7 +11,7 @@ MONGO_URI = process.env.MONGO_URI ? process.env.MONGO_URI : MONGO_URI;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoDB URI');
 }
-
+logger.info(`Connecting to: ${MONGO_URI}`);
 const connect = () => {
   mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
