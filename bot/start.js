377b9f8244643a26554e9b62a2c8d589f22c6590
Usman AR <usman8529@gmail.com>
2023-08-09 22:19:55 +0530

    /ban works with both username or telegramID

--- bot/start.js
+++ bot/start.js
@@ -509,6 +509,7 @@ const initialize = (botToken, options) => {
       username = username[0] == '@' ? username.slice(1) : username;
 
       let userTelegramId = username;
+      
       const user = await User.findOne({ $or: [{name : username},{tg_id:userTelegramId}] });
       if (!user) {
         await messages.notFoundUserMessage(ctx);
