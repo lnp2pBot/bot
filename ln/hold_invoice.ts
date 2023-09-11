40acc96150fb71d6a797c159a511a7801bbb8bf6
Mehrshad <code.rezaei@gmail.com>
2023-09-11 13:21:59 +0330

    ln: convert js to ts

--- ln/hold_invoice.ts
+++ ln/hold_invoice.ts
@@ -1,26 +1,26 @@
-const { createHash, randomBytes } = require('crypto');
-const lightning = require('lightning');
-const lnd = require('./connect');
-const logger = require('../logger');
+import { randomBytes, createHash } from 'crypto';
+import * as lightning from "lightning";
+import { lnd } from './connect'
+import logger from "../logger";
 
-const createHoldInvoice = async ({ description, amount }) => {
+const createHoldInvoice = async (description: string, amount: number ) => {
   try {
     const randomSecret = () => randomBytes(32);
-    const sha256 = buffer => createHash('sha256').update(buffer).digest('hex');
+    const sha256 = (buffer: Buffer): string => createHash('sha256').update(buffer).digest('hex');
     // We create a random secret
     const secret = randomSecret();
     const expiresAt = new Date();
     expiresAt.setSeconds(expiresAt.getSeconds() + 3600);
 
     const hash = sha256(secret);
-    const cltv_delta = parseInt(process.env.HOLD_INVOICE_CLTV_DELTA);
+    const cltv_delta = Number(process.env.HOLD_INVOICE_CLTV_DELTA);
     const { request, id } = await lightning.createHodlInvoice({
       cltv_delta,
       lnd,
       description,
       id: hash,
       tokens: amount,
-      expires_at: expiresAt,
+      expires_at: expiresAt.toISOString(),
     });
 
     // We sent back the response hash (id) to be used on testing
@@ -30,7 +30,7 @@ const createHoldInvoice = async ({ description, amount }) => {
   }
 };
 
-const settleHoldInvoice = async ({ secret }) => {
+const settleHoldInvoice = async ( { secret }: { secret: string } ) => {
   try {
     await lightning.settleHodlInvoice({ lnd, secret });
   } catch (error) {
@@ -38,7 +38,7 @@ const settleHoldInvoice = async ({ secret }) => {
   }
 };
 
-const cancelHoldInvoice = async ({ hash }) => {
+const cancelHoldInvoice = async ( { hash }: { hash: string } ) => {
   try {
     await lightning.cancelHodlInvoice({ lnd, id: hash });
   } catch (error) {
@@ -46,7 +46,7 @@ const cancelHoldInvoice = async ({ hash }) => {
   }
 };
 
-const getInvoice = async ({ hash }) => {
+const getInvoice = async ( { hash }: { hash: string } ) => {
   try {
     return await lightning.getInvoice({ lnd, id: hash });
   } catch (error) {
@@ -54,7 +54,7 @@ const getInvoice = async ({ hash }) => {
   }
 };
 
-module.exports = {
+export {
   createHoldInvoice,
   settleHoldInvoice,
   cancelHoldInvoice,
