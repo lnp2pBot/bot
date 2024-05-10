62825c2baca60191e55872203de6233398be84b9
Francisco Calderón <fjcalderon@gmail.com>
2024-05-10 18:06:49 -0300

    Generate random secret key in case we needed

--- bot/modules/nostr/config.js
+++ bot/modules/nostr/config.js
@@ -1,7 +1,7 @@
-const { getPublicKey } = require('nostr-tools/pure');
+const { getPublicKey, generateSecretKey } = require('nostr-tools/pure');
 const { SimplePool } = require('nostr-tools/pool');
 
-const sk = process.env.NOSTR_SK;
+const sk = process.env.NOSTR_SK || generateSecretKey();
 const pk = getPublicKey(sk);
 
 exports.getPrivateKey = () => sk;
