2fea4c860b817765479fc4daaee41d223220ade9
webwarrior <reg@webwarrior.ws>
2024-08-07 11:17:07 +0200

    bot/modules/language: convert to TS
    Convert bot/modules/language to TypeScript.

---
+++ bot/modules/language/actions.ts
@@ -0,0 +1,16 @@
+import { User } from '../../../models';
+import { MainContext } from '../../start';
+
+export const setLanguage = async (ctx: MainContext) => {
+  const tgId = (ctx.update as any).callback_query.from.id;
+  const user = await User.findOne({ tg_id: tgId });
+  if (user === null) return;
+  const code = ctx.match?.[1];
+  if (code === undefined)
+    throw new Error("setLanguage: code is undefined");
+  ctx.deleteMessage();
+  user.lang = code;
+  ctx.i18n.locale(code);
+  await user.save();
+  await ctx.reply(ctx.i18n.t('operation_successful'));
+};
---
+++ bot/modules/language/commands.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import fs from 'fs';
+import { getLanguageFlag } from '../../../util';
+import { logger } from '../../../logger';
+import { showFlagsMessage } from './messages';
+import { ILanguage } from '../../../util/languagesModel';
+import { MainContext } from '../../start';
+
+export const setlang = async (ctx: MainContext) => {
+  try {
+    const flags: ILanguage[] = [];
+    fs.readdirSync(path.join(__dirname, '../../../locales')).forEach(file => {
+      const lang = file.split('.')[0];
+      const flag = getLanguageFlag(lang);
+      if (flag !== undefined) {
+        flags.push(flag);
+      }
+    });
+    await showFlagsMessage(ctx, flags, ctx.user.lang);
+  } catch (error) {
+    logger.error(error);
+  }
+};
