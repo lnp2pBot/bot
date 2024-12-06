5e7b44ae2d7c8663f13eb4f9fdd3bf42bafda8d8
polaris <github.crimp560@passinbox.com>
2024-12-06 11:59:15 +0100

    Migrated block index.js to index.ts

--- bot/modules/block/index.ts
+++ bot/modules/block/index.ts
@@ -1,15 +1,17 @@
+import { Telegraf } from 'telegraf';
+
 const commands = require('./commands');
 const { userMiddleware } = require('../../middleware/user');
 
-exports.configure = bot => {
+exports.configure = (bot: Telegraf) => {
   bot.command('block', userMiddleware, async (ctx, next) => {
-    const args = ctx.message.text.split(' ');
+    const args = ctx.message.text.split(' ') || [];
     if (args.length !== 2) return next();
     commands.block(ctx, args[1]);
   });
 
   bot.command('unblock', userMiddleware, async (ctx, next) => {
-    const args = ctx.message.text.split(' ');
+    const args = ctx.message.text.split(' ') || [];
     if (args.length !== 2) return next();
     commands.unblock(ctx, args[1]);
   });
