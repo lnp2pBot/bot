67cf2557be50bbc90be891aa583140b9df19d1db
webwarrior-ws <webwarrior-ws@users.noreply.github.com>
2025-01-23 14:13:01 +0100

    bot/modules/language: convert to TS (#626)
    Convert bot/modules/language to TypeScript.

--- bot/modules/language/index.js
+++
@@ -1,8 +0,0 @@
-const { userMiddleware } = require('../../middleware/user');
-const commands = require('./commands');
-const actions = require('./actions');
-
-exports.configure = bot => {
-  bot.command('setlang', userMiddleware, commands.setlang);
-  bot.action(/^setLanguage_([a-z]{2})$/, userMiddleware, actions.setLanguage);
-};
