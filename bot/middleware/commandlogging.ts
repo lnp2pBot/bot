3a335ad4c212de43b0004136609f344fd872c1ad
Lucas Jeffrey <luquijeffrey@gmail.com>
2026-01-05 18:20:46 -0300

    Code formatting and added a configuration variable of the max size of the command log file

--- bot/middleware/commandlogging.ts
+++ bot/middleware/commandlogging.ts
@@ -3,68 +3,73 @@ import { CommunityContext } from '../modules/community/communityContext';
 import winston from 'winston';
 
 const logFile = process.env.COMMAND_LOG_FILE || 'commands.log';
+const maxSizeGB = parseInt(process.env.COMMAND_LOG_SIZE_GB || '5', 10) || 5;
 
 const logger = winston.createLogger({
-	format: winston.format.combine(
-		winston.format.timestamp({
-			format: 'YYYY-MM-DDTHH:mm:ss.SSSZ',
-		}),
-		winston.format.printf(info => {
-			return `[${info.timestamp}] ${info.level}: ${info.message} ${
-				info.stack ? info.stack : ''
-			}`;
-		}),
-	),
-	levels: winston.config.syslog.levels,
-	level: 'debug',
-	transports: [
-		new winston.transports.File({
-			filename: logFile,
-			maxsize: 5 * 1024 * 1024 * 1000, // 5GB
-		}),
-	],
-	exitOnError: false,
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DDTHH:mm:ss.SSSZ',
+    }),
+    winston.format.printf(info => {
+      return `[${info.timestamp}] ${info.level}: ${info.message} ${
+        info.stack ? info.stack : ''
+      }`;
+    }),
+  ),
+  levels: winston.config.syslog.levels,
+  level: 'debug',
+  transports: [
+    new winston.transports.File({
+      filename: logFile,
+      maxsize: maxSizeGB * 1024 * 1024 * 1000, // 5GB
+    }),
+  ],
+  exitOnError: false,
 });
 
 export function commandLogger(): MiddlewareFn<CommunityContext> {
-	return async (ctx, next) => {
-		try {
-			if (ctx.message && 'text' in ctx.message) {
-				const msg = ctx.message;
-				const text = msg.text.trim();
-				const userId = msg.from?.id ?? 'unknown';
+  return async (ctx, next) => {
+    try {
+      if (ctx.message && 'text' in ctx.message) {
+        const msg = ctx.message;
+        const text = msg.text.trim();
+        const userId = msg.from?.id ?? 'unknown';
 
-				let command: string | null = null;
-				let args: string[] = [];
-				let isCommand: boolean;
+        let command: string | null = null;
+        let args: string[] = [];
+        let isCommand: boolean;
 
-				if (text.startsWith('/')) {
-					const parts = text.split(/\s+/);
-					command = parts[0];
-					args = parts.slice(1);
-					isCommand = true;
-				} else {
-					isCommand = false;
-					command = text;
-				}
+        if (text.startsWith('/')) {
+          const parts = text.split(/\s+/);
+          command = parts[0];
+          args = parts.slice(1);
+          isCommand = true;
+        } else {
+          isCommand = false;
+          command = text;
+        }
 
-				const userName = msg.from?.username ?? '';
+        const userName = msg.from?.username ?? '';
 
-				logger.info(`User @${userName} [${userId}] ${isCommand? 'executed command:' : 'sent message:'} ${command} with args: [${args.join(', ')}]`);
-			} else if (ctx.callbackQuery && 'data' in ctx.callbackQuery) {
-				// Attempt to get message text
-				const msgText = (ctx.callbackQuery?.message as any)?.text ?? '';
-				const callbackData = ctx.callbackQuery.data;
-				const userName = ctx.callbackQuery.from?.username ?? '';
-				const userId = ctx.callbackQuery.from?.id ?? '';
-				logger.info(`User @${userName} [${userId}] sent callback query with data: ${callbackData}. Message text: '${msgText}'`);
-			} else {
-				logger.info(`Received non-command message or update from user.`);
-			}
-		} catch (err) {
-			logger.error('logging middleware failed', err);
-		}
+        logger.info(
+          `User @${userName} [${userId}] ${isCommand ? 'executed command:' : 'sent message:'} ${command} with args: [${args.join(', ')}]`,
+        );
+      } else if (ctx.callbackQuery && 'data' in ctx.callbackQuery) {
+        // Attempt to get message text
+        const msgText = (ctx.callbackQuery?.message as any)?.text ?? '';
+        const callbackData = ctx.callbackQuery.data;
+        const userName = ctx.callbackQuery.from?.username ?? '';
+        const userId = ctx.callbackQuery.from?.id ?? '';
+        logger.info(
+          `User @${userName} [${userId}] sent callback query with data: ${callbackData}. Message text: '${msgText}'`,
+        );
+      } else {
+        logger.info(`Received non-command message or update from user.`);
+      }
+    } catch (err) {
+      logger.error('logging middleware failed', err);
+    }
 
-		return next();
-	};
+    return next();
+  };
 }
