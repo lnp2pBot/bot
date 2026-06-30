73d0f424470ce47fb30b92715286a86596865a3f
Maxi Vila <matovi98@gmail.com>
2026-06-30 16:07:18 -0300

    fix: remove _id: string override from IScheduledOrder to satisfy strict Mongoose Document typing

--- models/scheduled_order.ts
+++ models/scheduled_order.ts
@@ -1,7 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IScheduledOrder extends Document {
-  _id: string;
   creator_id: string;
   type: string;
   amount: number;
